05e4490fe1b707d721ad47b0419a3919ed88e1f1
Alejandro Canizares <63413161+alejocv7@users.noreply.github.com>
2026-01-19 20:36:52 -0500

    Add fastapi quick setup and health endpoint

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI
+from .routers.health import router as health_router
+
+app = FastAPI(title="API Analytics")
+app.include_router(health_router)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}

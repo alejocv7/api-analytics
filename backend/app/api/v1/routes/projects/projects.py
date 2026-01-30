fffdc8c3111a1d9ed53899be59ad9c5fe592cdd4
Alejandro Canizares <63413161+alejocv7@users.noreply.github.com>
2026-01-30 10:40:13 -0500

    Async project

--- backend/app/api/v1/routes/projects/projects.py
+++ backend/app/api/v1/routes/projects/projects.py
@@ -8,24 +8,24 @@ router = APIRouter()
 
 
 @router.get("/", response_model=list[schemas.ProjectResponse])
-def get_projects(user: CurrentUserDep, session: SessionDep):
+async def get_projects(user: CurrentUserDep, session: SessionDep):
     return project.get_user_projects(user.id, session)
 
 
 @router.post("/", response_model=schemas.ProjectResponse)
-def create_project(
+async def create_project(
     project_in: schemas.ProjectCreate, user: CurrentUserDep, session: SessionDep
 ):
     return project.create_user_project(user.id, project_in, session)
 
 
 @router.get("/{project_key}", response_model=schemas.ProjectResponse)
-def get_project(project_key: str, user: CurrentUserDep, session: SessionDep):
+async def get_project(project_key: str, user: CurrentUserDep, session: SessionDep):
     return project.get_user_project_by_key(project_key, user.id, session)
 
 
 @router.put("/{project_key}", response_model=schemas.ProjectResponse)
-def update_project(
+async def update_project(
     project_key: str,
     update_data: schemas.ProjectUpdate,
     user: CurrentUserDep,
@@ -35,5 +35,5 @@ def update_project(
 
 
 @router.delete("/{project_key}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_project(project_key: str, user: CurrentUserDep, session: SessionDep):
+async def delete_project(project_key: str, user: CurrentUserDep, session: SessionDep):
     project.delete_user_project(user.id, project_key, session)
